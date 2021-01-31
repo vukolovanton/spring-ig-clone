fbb2622b15ba10be7ec0b0e72fd9ab5748e81d11
vukolovanton <vukolovanton92@gmail.com>
2021-01-31 08:41:12 -0500

    Security JWT configuration

---
+++ src/main/java/com/practice/ig/security/UserDetailsService.java
@@ -0,0 +1,17 @@
+package com.practice.ig.security;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class UserDetailsService implements org.springframework.security.core.userdetails.UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return new User("foo", "bar", new ArrayList<>());
+    }
+}
